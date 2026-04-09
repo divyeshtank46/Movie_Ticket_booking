746a6fdc64ba833b62967334f89df5fdff0163dd
divyeshtank46 <assc0760@gmail.com>
2026-04-09 19:52:43 +0530

    web-icon added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,4 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './styles/index.css'
 import './styles/index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
