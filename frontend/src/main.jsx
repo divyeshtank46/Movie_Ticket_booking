2ea88ebc5da674543974e155804191960393dc35
divyeshtank46 <assc0760@gmail.com>
2026-04-15 19:18:43 +0530

    fix cors options error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,9 @@ import './styles/index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import 'react-toastify/dist/ReactToastify.css';
-import { GoogleOAuthProvider} from "@react-oauth/google"
+import { GoogleOAuthProvider } from "@react-oauth/google"
+import axios from "axios";
+axios.defaults.withCredentials = true;
 createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
     <BrowserRouter>
