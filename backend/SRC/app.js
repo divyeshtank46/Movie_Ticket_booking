fe233033cd1155168afea4889deafbe937752838
divyeshtank46 <assc0760@gmail.com>
2026-04-15 19:20:56 +0530

    changed

--- backend/SRC/app.js
+++ backend/SRC/app.js
@@ -22,12 +22,7 @@ app.use(cors({
     origin: "https://movie-ticket-booking-seven-brown.vercel.app",
     credentials: true
 }));
-app.options("/", cors());
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://movie-ticket-booking-seven-brown.vercel.app");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
+
 
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
