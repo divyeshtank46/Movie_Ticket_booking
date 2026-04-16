eb6aa1c33114481e506a02bf4172254f88d65e51
divyeshtank46 <assc0760@gmail.com>
2026-04-16 18:24:20 +0530

    commit

--- backend/SRC/Controllers/payment.Controller.js
+++ backend/SRC/Controllers/payment.Controller.js
@@ -17,7 +17,7 @@ const createOrder = async (req, res) => {
         });
 
     } catch (error) {
-        console.log("ENV CHECK:", process.env.RAZORPAY_KEY_ID);
+        console.log("SECRET:", process.env.RAZORPAY_KEY_SECRET);
         return res.status(500).json({
             success: false,
             message: `Order Creation Failed ${error.message}`
