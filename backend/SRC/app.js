73aa894e81e9c84301c59f2ff5225f9eded65642
divyeshtank46 <assc0760@gmail.com>
2026-05-11 18:55:09 +0530

    port

--- backend/SRC/app.js
+++ backend/SRC/app.js
@@ -1,26 +1,32 @@
-const express = require('express');
-const authRoutes = require('./Routes/auth.routes');
-const cors = require("cors")
-const cookieParser = require('cookie-parser');
-const cinemaroutes = require('./Routes/cinema.routes')
-const movieroutes = require('./Routes/movie.routes')
-const showroutes = require('./Routes/show.routes');
-const bookingroutes = require('./Routes/booking.routes');
-const paymentroutes = require('./Routes/payment.routes');
+const express = require("express");
+const authRoutes = require("./Routes/auth.routes");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const cinemaroutes = require("./Routes/cinema.routes");
+const movieroutes = require("./Routes/movie.routes");
+const showroutes = require("./Routes/show.routes");
+const bookingroutes = require("./Routes/booking.routes");
+const paymentroutes = require("./Routes/payment.routes");
 const app = express();
 app.use(express.json());
 
 app.set("trust proxy", 1);
-app.use(cors({
-    origin: "https://movie-ticket-booking-seven-brown.vercel.app","http://localhost:5173/"
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "https://movie-ticket-booking-seven-brown.vercel.app",
+      "http://localhost:5173/",
+    ],
+
+    credentials: true,
+  }),
+);
 
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/movie", movieroutes);
-app.use("/api/cinema", cinemaroutes)
-app.use("/api/ticket", bookingroutes)
+app.use("/api/cinema", cinemaroutes);
+app.use("/api/ticket", bookingroutes);
 app.use("/api/show", showroutes);
-app.use("/api/payment", paymentroutes)
+app.use("/api/payment", paymentroutes);
 module.exports = app;
